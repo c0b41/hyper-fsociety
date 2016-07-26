5e291440e2391f145bc97634b7e783489ab58825
ayhankuru <ayhankuru@yandex.com.tr>
2016-07-27 02:16:57 +0300

    init

---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict';
+const foregroundColor = '#A9BCD0';
+const backgroundColor = '#1B1B1E';
+const black = backgroundColor;
+const red = '#ff5c57';
+const green = '#5af78e';
+const yellow = '#f3f99d';
+const blue = '#57c7ff';
+const magenta = '#ff6ac1';
+const cyan = '#9aedfe';
+const gray = '#f1f1f0';
+const brightBlack = '#B0BEC5';
+const brightWhite = foregroundColor;
+
+exports.decorateConfig = config => {
+  return Object.assign({}, config, {
+    backgroundColor,
+    foregroundColor,
+    borderColor: '#D8DBE2',
+    cursorColor: '#D8DBE2',
+    fontSize:14,
+    FontFamily:'Fira Code, Menlo, DejaVu Sans Mono, Lucida Console, monospace'
+    colors: [
+      black,
+      red,
+      green,
+      yellow,
+      blue,
+      magenta,
+      cyan,
+      gray,
+      brightBlack,
+      red,
+      green,
+      yellow,
+      blue,
+      magenta,
+      cyan,
+      brightWhite
+    ],
+    css: `
+      ${config.css || ''}
+      .tab_tab {
+        border: 0 !important;
+      }
+      .tab_active{
+        background-color: #373F51;
+      }
+      .tab_active:before {
+        border-color: #2A99CF;
+      }
+    `
+  });
+}
+
+exports.onWindow = (window) => {
+  if(process.platform === 'linux') {
+    window.setAutoHideMenuBar(true);
+  }
+}
